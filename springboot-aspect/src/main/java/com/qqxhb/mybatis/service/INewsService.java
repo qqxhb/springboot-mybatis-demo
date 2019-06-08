559372d2d936be9359be287587c2bf908b2c1fd1
qqxhb <1252905006@qq.com>
2019-06-08 12:23:00 +0800

    aspect

---
+++ springboot-aspect/src/main/java/com/qqxhb/mybatis/service/INewsService.java
@@ -0,0 +1,16 @@
+package com.qqxhb.mybatis.service;
+
+import com.qqxhb.mybatis.entity.News;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author qqxhb
+ * @since 2019-05-10
+ */
+public interface INewsService extends IService<News> {
+
+}
